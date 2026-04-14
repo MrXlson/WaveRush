70c1f84a032d1771636f791e0b22d24ee9dfc1cb
MrXlson <dominikdrasar39@gmail.com>
2026-04-14 10:56:36 +0200

    Arena.java

--- src/main/java/me/plugin/waverush/model/Arena.java
+++ src/main/java/me/plugin/waverush/model/Arena.java
@@ -1,1 +1,21 @@
+package me.plugin.waverush.model;
 
+import org.bukkit.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arena {
+
+    public String name;
+    public Location pos1;
+    public Location pos2;
+    public Location playerSpawn;
+    public List<Location> mobSpawns = new ArrayList<>();
+
+    public List<org.bukkit.entity.Player> players = new ArrayList<>();
+
+    public Arena(String name) {
+        this.name = name;
+    }
+}
