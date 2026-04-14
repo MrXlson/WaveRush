e2afc5f589c685fe8776650967454fe6dfd9e7aa
MrXlson <dominikdrasar39@gmail.com>
2026-04-14 11:08:00 +0200

    ArenaListener.java

--- src/main/java/me/plugin/waverush/listener/ArenaListener.java
+++ src/main/java/me/plugin/waverush/listener/ArenaListener.java
@@ -1,7 +1,13 @@
 package me.plugin.waverush.listener;
 
+import me.plugin.waverush.manager.ArenaManager;
 import org.bukkit.event.Listener;
 
 public class ArenaListener implements Listener {
 
+    private final ArenaManager manager;
+
+    public ArenaListener(ArenaManager manager) {
+        this.manager = manager;
+    }
 }
