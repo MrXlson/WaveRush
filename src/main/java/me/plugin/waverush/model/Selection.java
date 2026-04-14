81ad94942c7399d80bb96f1d1ce59738a370346b
MrXlson <dominikdrasar39@gmail.com>
2026-04-14 10:56:07 +0200

    Selection.java

--- src/main/java/me/plugin/waverush/model/Selection.java
+++ src/main/java/me/plugin/waverush/model/Selection.java
@@ -1,1 +1,14 @@
+package me.plugin.waverush.model;
 
+import org.bukkit.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Selection {
+
+    public Location pos1;
+    public Location pos2;
+    public Location playerSpawn;
+    public List<Location> mobSpawns = new ArrayList<>();
+}
