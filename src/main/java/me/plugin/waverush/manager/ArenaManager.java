53b2e075299bb8dbdafa644567ca9005501a7408
MrXlson <dominikdrasar39@gmail.com>
2026-04-14 10:57:48 +0200

    ArenaManager.java

--- src/main/java/me/plugin/waverush/manager/ArenaManager.java
+++ src/main/java/me/plugin/waverush/manager/ArenaManager.java
@@ -1,1 +1,19 @@
+package me.plugin.waverush.manager;
 
+import me.plugin.waverush.model.Arena;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArenaManager {
+
+    private final Map<String, Arena> arenas = new HashMap<>();
+
+    public void addArena(Arena arena) {
+        arenas.put(arena.name.toLowerCase(), arena);
+    }
+
+    public Arena getArena(String name) {
+        return arenas.get(name.toLowerCase());
+    }
+}
