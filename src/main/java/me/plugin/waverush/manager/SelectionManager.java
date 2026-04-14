30a1160dd6d3606abdc4dc65de0bb965b6817830
MrXlson <dominikdrasar39@gmail.com>
2026-04-14 10:57:10 +0200

    SelectionManager.java

--- src/main/java/me/plugin/waverush/manager/SelectionManager.java
+++ src/main/java/me/plugin/waverush/manager/SelectionManager.java
@@ -1,1 +1,16 @@
+package me.plugin.waverush.manager;
 
+import me.plugin.waverush.model.Selection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class SelectionManager {
+
+    private final Map<UUID, Selection> selections = new HashMap<>();
+
+    public Selection get(UUID uuid) {
+        return selections.computeIfAbsent(uuid, k -> new Selection());
+    }
+}
