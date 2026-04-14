0c0312d26ffda501e5d19a93da94a06c8927344e
MrXlson <dominikdrasar39@gmail.com>
2026-04-14 23:10:00 +0200

    ArenaManager.java

--- src/main/java/me/plugin/waverush/manager/ArenaManager.java
+++ src/main/java/me/plugin/waverush/manager/ArenaManager.java
@@ -1,6 +1,7 @@
 package me.plugin.waverush.manager;
 
 import me.plugin.waverush.model.Arena;
+import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +9,7 @@ import java.util.Map;
 public class ArenaManager {
 
     private final Map<String, Arena> arenas = new HashMap<>();
+    private final Map<Player, Arena> playing = new HashMap<>();
 
     public void createArena(String name, Arena arena) {
         arenas.put(name.toLowerCase(), arena);
@@ -15,5 +17,23 @@ public class ArenaManager {
 
     public Arena getArena(String name) {
         return arenas.get(name.toLowerCase());
+    }
+
+    public void joinArena(Player player, String name) {
+        Arena arena = getArena(name);
+        if (arena == null) return;
+
+        playing.put(player, arena);
+
+        // teleport na střed arény
+        player.teleport(arena.getPos1());
+    }
+
+    public boolean isPlaying(Player player) {
+        return playing.containsKey(player);
+    }
+
+    public void leave(Player player) {
+        playing.remove(player);
     }
 }
