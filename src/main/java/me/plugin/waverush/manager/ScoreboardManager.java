bb42e148a362a8f3a490050ef22e772d3604fbe6
MrXlson <dominikdrasar39@gmail.com>
2026-04-15 00:18:37 +0200

    ScoreboardManager.java

--- src/main/java/me/plugin/waverush/manager/ScoreboardManager.java
+++ src/main/java/me/plugin/waverush/manager/ScoreboardManager.java
@@ -1,1 +1,23 @@
+package me.plugin.waverush.manager;
 
+import me.plugin.waverush.game.GameTask;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.*;
+
+public class ScoreboardManager {
+
+    public static void update(Player player, int wave) {
+
+        org.bukkit.scoreboard.ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+
+        Objective obj = board.registerNewObjective("wave", "dummy", "§6WaveRush");
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+        obj.getScore("§7Wave: §e" + wave).setScore(2);
+        obj.getScore("§7Kills: §a" + GameTask.getKills(player)).setScore(1);
+
+        player.setScoreboard(board);
+    }
+}
