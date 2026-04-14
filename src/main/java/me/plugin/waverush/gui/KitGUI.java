60afacc0999244e12d9909e8a70c97498743244c
MrXlson <dominikdrasar39@gmail.com>
2026-04-15 00:16:16 +0200

    KitGUI.java

--- src/main/java/me/plugin/waverush/gui/KitGUI.java
+++ src/main/java/me/plugin/waverush/gui/KitGUI.java
@@ -1,1 +1,29 @@
+package me.plugin.waverush.gui;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class KitGUI {
+
+    public static void open(Player player) {
+        Inventory inv = Bukkit.createInventory(null, 9, "§8Výběr Kitu");
+
+        inv.setItem(2, createItem(Material.IRON_SWORD, "§aWarrior"));
+        inv.setItem(4, createItem(Material.BOW, "§aArcher"));
+        inv.setItem(6, createItem(Material.BLAZE_ROD, "§aMage"));
+
+        player.openInventory(inv);
+    }
+
+    private static ItemStack createItem(Material mat, String name) {
+        ItemStack item = new ItemStack(mat);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        item.setItemMeta(meta);
+        return item;
+    }
+}
