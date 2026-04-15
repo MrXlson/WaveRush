75fbf96c05b372dd79b1e5f2b16510475bf03302
MrXlson <dominikdrasar39@gmail.com>
2026-04-15 09:41:59 +0200

    DeathListener.java

--- src/main/java/me/plugin/waverush/listener/DeathListener.java
+++ src/main/java/me/plugin/waverush/listener/DeathListener.java
@@ -1,7 +1,8 @@
 package me.plugin.waverush.listener;
 
-import me.plugin.waverush.WaveRushPlugin;
-import org.bukkit.ChatColor;
+import me.plugin.waverush.manager.ArenaManager;
+import me.plugin.waverush.model.Arena;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,15 +10,36 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class DeathListener implements Listener {
 
+    private final ArenaManager arenaManager;
+
+    public DeathListener(ArenaManager arenaManager) {
+        this.arenaManager = arenaManager;
+    }
+
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
         Player player = e.getEntity();
 
-        player.sendMessage(ChatColor.RED + "Prohrál jsi!");
-        player.sendTitle("§cPROHRA", "§7Zkus to znovu!", 10, 60, 10);
+        Arena arena = arenaManager.getArena(player);
+        if (arena == null) return;
 
-        WaveRushPlugin.getPlugin(WaveRushPlugin.class)
-                .getArenaManager()
-                .leave(player);
+        // 🔥 odeber hráče z arény
+        arena.removePlayer(player);
+
+        Bukkit.getScheduler().runTaskLater(
+                Bukkit.getPluginManager().getPlugin("WaveRush"),
+                () -> player.spigot().respawn(),
+                2L
+        );
+
+        // 🔥 teleport do lobby
+        arenaManager.sendToLobby(player);
+
+        player.sendMessage("§cZemřel jsi!");
+
+        // 🔥 kontrola konce hry
+        if (arena.getPlayers().isEmpty()) {
+            arena.endGame(false); // lose
+        }
     }
 }
