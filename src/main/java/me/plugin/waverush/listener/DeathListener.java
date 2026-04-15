4893d045a29fccaa94178f0f62136eb9546dddff
MrXlson <dominikdrasar39@gmail.com>
2026-04-15 10:41:33 +0200

    DeathListener.java

--- src/main/java/me/plugin/waverush/listener/DeathListener.java
+++ src/main/java/me/plugin/waverush/listener/DeathListener.java
@@ -1,11 +1,8 @@
 package me.plugin.waverush.listener;
 
 import me.plugin.waverush.manager.ArenaManager;
-import me.plugin.waverush.model.Arena;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import org.bukkit.event.*;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class DeathListener implements Listener {
@@ -20,26 +17,8 @@ public class DeathListener implements Listener {
     public void onDeath(PlayerDeathEvent e) {
         Player player = e.getEntity();
 
-        Arena arena = arenaManager.getArena(player);
-        if (arena == null) return;
+        if (!arenaManager.isInArena(player)) return;
 
-        // 🔥 odeber hráče z arény
-        arena.removePlayer(player);
-
-        Bukkit.getScheduler().runTaskLater(
-                Bukkit.getPluginManager().getPlugin("WaveRush"),
-                () -> player.spigot().respawn(),
-                2L
-        );
-
-        // 🔥 teleport do lobby
         arenaManager.sendToLobby(player);
-
-        player.sendMessage("§cZemřel jsi!");
-
-        // 🔥 kontrola konce hry
-        if (arena.getPlayers().isEmpty()) {
-            arena.endGame(false); // lose
-        }
     }
 }
