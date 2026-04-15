11566532648477935160df5dd205edb11933e1ef
MrXlson <dominikdrasar39@gmail.com>
2026-04-15 10:12:58 +0200

    SignListener.java

--- src/main/java/me/plugin/waverush/listener/SignListener.java
+++ src/main/java/me/plugin/waverush/listener/SignListener.java
@@ -2,10 +2,9 @@ package me.plugin.waverush.listener;
 
 import me.plugin.waverush.manager.ArenaManager;
 import org.bukkit.block.Sign;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class SignListener implements Listener {
@@ -17,19 +16,35 @@ public class SignListener implements Listener {
     }
 
     @EventHandler
-    public void onClick(PlayerInteractEvent e) {
-        if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+    public void onSignCreate(SignChangeEvent e) {
 
+        if (!e.getLine(0).equalsIgnoreCase("[WaveRush]")) return;
+
+        String arena = e.getLine(1);
+
+        arenaManager.addSign(e.getBlock().getLocation());
+
+        e.setLine(0, "§b[WaveRush]");
+        e.setLine(2, "§aWAITING");
+        e.setLine(3, "§b0 hráčů");
+
+        e.getPlayer().sendMessage("§aCedulka vytvořena!");
+    }
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent e) {
+
+        if (e.getClickedBlock() == null) return;
         if (!(e.getClickedBlock().getState() instanceof Sign)) return;
 
         Sign sign = (Sign) e.getClickedBlock().getState();
 
-        if (!sign.getLine(0).equalsIgnoreCase("[WaveRush]")) return;
+        if (!sign.getLine(0).contains("WaveRush")) return;
 
         String arenaName = sign.getLine(1);
 
-        Player player = e.getPlayer(); // 🔥 FIX
-
-        arenaManager.joinArena(player, arenaName);
+        if (!arenaManager.joinFirstAvailable(e.getPlayer())) {
+            e.getPlayer().sendMessage("§cŽádná dostupná aréna!");
+        }
     }
 }
