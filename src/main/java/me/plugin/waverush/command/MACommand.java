d96f5f378186aa87281a8ce5f8b45667c4c02aaf
MrXlson <dominikdrasar39@gmail.com>
2026-04-15 10:40:32 +0200

    MACommand.java

--- src/main/java/me/plugin/waverush/command/MACommand.java
+++ src/main/java/me/plugin/waverush/command/MACommand.java
@@ -3,114 +3,56 @@ package me.plugin.waverush.command;
 import me.plugin.waverush.WaveRushPlugin;
 import me.plugin.waverush.manager.ArenaManager;
 import me.plugin.waverush.manager.SelectionManager;
-import me.plugin.waverush.model.Arena;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 
 public class MACommand implements CommandExecutor {
 
+    private final ArenaManager arenaManager;
+    private final SelectionManager selectionManager;
+    private final WaveRushPlugin plugin;
+
+    public MACommand(ArenaManager arenaManager, SelectionManager selectionManager, WaveRushPlugin plugin) {
+        this.arenaManager = arenaManager;
+        this.selectionManager = selectionManager;
+        this.plugin = plugin;
+    }
+
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Pouze hráč!");
-            return true;
-        }
-
-        Player player = (Player) sender;
-
-        WaveRushPlugin plugin = (WaveRushPlugin) Bukkit.getPluginManager().getPlugin("WaveRush");
-        ArenaManager arenaManager = plugin.getArenaManager();
-        SelectionManager selectionManager = plugin.getSelectionManager();
-
-        boolean isAdmin = player.hasPermission("waverush.admin");
+        if (!(sender instanceof Player player)) return true;
 
         if (args.length == 0) {
-            player.sendMessage("§ePoužití: /ma <menu|kit|join|create|select|list|reload>");
+            player.sendMessage("§cPoužij /ma join <arena>");
             return true;
         }
 
-        // 🔥 RELOAD
-        if (args[0].equalsIgnoreCase("reload")) {
-            if (!player.hasPermission("waverush.reload")) {
-                player.sendMessage("§cNemáš oprávnění!");
-                return true;
-            }
-
-            plugin.reloadPluginConfig();
-            player.sendMessage("§aConfig byl reloadnut!");
-            return true;
-        }
-
-        // 🔥 JOIN
         if (args[0].equalsIgnoreCase("join")) {
-            if (!isAdmin && !player.hasPermission("waverush.join")) {
-                player.sendMessage("§cNemáš oprávnění!");
-                return true;
-            }
 
             if (args.length < 2) {
-                player.sendMessage("§cPoužití: /ma join <arena>");
+                player.sendMessage("§cZadej arénu!");
                 return true;
             }
 
-            arenaManager.joinArena(player, args[1]);
-            return true;
+            boolean success = arenaManager.joinArena(player, args[1]);
+
+            if (!success) {
+                player.sendMessage("§cAréna neexistuje!");
+            } else {
+                player.sendMessage("§aPřipojen!");
+            }
         }
 
-        // 🔥 CREATE
-        if (args[0].equalsIgnoreCase("create")) {
-            if (!isAdmin && !player.hasPermission("waverush.create")) {
-                player.sendMessage("§cNemáš oprávnění!");
-                return true;
-            }
-
-            if (args.length < 2) {
-                player.sendMessage("§cPoužití: /ma create <název>");
-                return true;
-            }
-
-            String name = args[1];
-
-            Location pos1 = selectionManager.getPos1(player);
-            Location pos2 = selectionManager.getPos2(player);
-
-            if (pos1 == null || pos2 == null) {
-                player.sendMessage("§cMusíš označit 2 pozice!");
-                return true;
-            }
-
-            Arena arena = new Arena(name, pos1, pos2);
-            arenaManager.createArena(name, arena);
-
-            player.sendMessage("§aAréna vytvořena: " + name);
-            return true;
-        }
-
-        // 🔥 SELECT
-        if (args[0].equalsIgnoreCase("select")) {
-            if (!isAdmin && !player.hasPermission("waverush.select")) {
-                player.sendMessage("§cNemáš oprávnění!");
-                return true;
-            }
-
-            player.sendMessage("§aPoužij motyku pro označení pozic!");
-            return true;
-        }
-
-        // 🔥 LIST
         if (args[0].equalsIgnoreCase("list")) {
-            player.sendMessage("§eArény:");
-
-            for (Arena arena : arenaManager.getArenas().values()) {
-                player.sendMessage("§7- " + arena.getName());
+            for (var arena : arenaManager.getArenas()) {
+                player.sendMessage("§e" + arena.getName());
             }
+        }
 
-            return true;
+        if (args[0].equalsIgnoreCase("reload")) {
+            plugin.reloadPluginConfig();
+            player.sendMessage("§aReload hotový!");
         }
 
         return true;
