10d53b0de44e04c6315d6d9bc794e8d227caec68
MrXlson <dominikdrasar39@gmail.com>
2026-04-15 10:13:48 +0200

    SignUpdateTask.java

--- src/main/java/me/plugin/waverush/task/SignUpdateTask.java
+++ src/main/java/me/plugin/waverush/task/SignUpdateTask.java
@@ -1,9 +1,7 @@
 package me.plugin.waverush.task;
 
 import me.plugin.waverush.manager.ArenaManager;
-import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
-import org.bukkit.block.Block;
+import org.bukkit.Location;
 import org.bukkit.block.Sign;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -18,38 +16,23 @@ public class SignUpdateTask extends BukkitRunnable {
     @Override
     public void run() {
 
-        Bukkit.getWorlds().forEach(world -> {
-            for (Chunk chunk : world.getLoadedChunks()) {
+        for (Location loc : arenaManager.getSigns()) {
 
-                for (int x = 0; x < 16; x++) {
-                    for (int y = 0; y < world.getMaxHeight(); y++) {
-                        for (int z = 0; z < 16; z++) {
+            if (!(loc.getBlock().getState() instanceof Sign)) continue;
 
-                            Block block = chunk.getBlock(x, y, z);
+            Sign sign = (Sign) loc.getBlock().getState();
 
-                            if (!(block.getState() instanceof Sign)) continue;
+            String arenaName = sign.getLine(1);
+            int players = arenaManager.getPlayerCount(arenaName);
 
-                            Sign sign = (Sign) block.getState();
+            String status = "§aWAITING";
+            if (players >= 4) status = "§cFULL";
+            else if (players > 0) status = "§eINGAME";
 
-                            if (!sign.getLine(0).equalsIgnoreCase("[WaveRush]")) continue;
+            sign.setLine(2, status);
+            sign.setLine(3, "§b" + players + " hráčů");
 
-                            String arenaName = sign.getLine(1);
-
-                            int players = arenaManager.getPlayerCount(arenaName);
-
-                            // 🔥 STATUS
-                            String status = "§aWAITING";
-                            if (players >= 4) status = "§cFULL";
-                            else if (players > 0) status = "§eINGAME";
-
-                            sign.setLine(2, status);
-                            sign.setLine(3, "§b" + players + " hráčů");
-
-                            sign.update();
-                        }
-                    }
-                }
-            }
-        });
+            sign.update();
+        }
     }
 }
