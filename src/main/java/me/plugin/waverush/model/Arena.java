3aaee89994b99adae47b89dbf66e6a791647e702
MrXlson <dominikdrasar39@gmail.com>
2026-04-15 11:54:37 +0200

    Arena.java

--- src/main/java/me/plugin/waverush/model/Arena.java
+++ src/main/java/me/plugin/waverush/model/Arena.java
@@ -1,6 +1,6 @@
 package me.plugin.waverush.model;
 
-import me.plugin.waverush.task.GameTask;
+import me.plugin.waverush.game.GameTask;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
