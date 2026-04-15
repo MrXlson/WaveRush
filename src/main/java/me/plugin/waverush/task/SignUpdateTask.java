b84e774c18e4c86ce21ad5ec9db6448b55b93c8e
MrXlson <dominikdrasar39@gmail.com>
2026-04-15 09:51:56 +0200

    SignUpdateTask.java

--- src/main/java/me/plugin/waverush/task/SignUpdateTask.java
+++ src/main/java/me/plugin/waverush/task/SignUpdateTask.java
@@ -1,8 +1,8 @@
 package me.plugin.waverush.task;
 
-import me.plugin.waverush.WaveRushPlugin;
 import me.plugin.waverush.manager.ArenaManager;
 import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -10,37 +10,38 @@ public class SignUpdateTask extends BukkitRunnable {
 
     private final ArenaManager arenaManager;
 
-    public SignUpdateTask() {
-        this.arenaManager = WaveRushPlugin.getPlugin(WaveRushPlugin.class).getArenaManager();
+    public SignUpdateTask(ArenaManager arenaManager) {
+        this.arenaManager = arenaManager;
     }
 
     @Override
     public void run() {
 
         Bukkit.getWorlds().forEach(world -> {
+            world.getLoadedChunks().forEach(chunk -> {
+                for (Block block : chunk.getBlocks()) {
 
-            for (var chunk : world.getLoadedChunks()) {
-                for (var blockState : chunk.getTileEntities()) {
+                    if (!(block.getState() instanceof Sign)) continue;
 
-                    if (!(blockState instanceof Sign sign)) continue;
+                    Sign sign = (Sign) block.getState();
 
-                    if (!sign.getLine(0).contains("[WaveRush]")) continue;
+                    if (!sign.getLine(0).equalsIgnoreCase("[WaveRush]")) continue;
 
-                    String arena = org.bukkit.ChatColor.stripColor(sign.getLine(1));
+                    String arenaName = sign.getLine(1);
 
-                    int players = arenaManager.getPlayerCount(arena);
+                    int players = arenaManager.getPlayerCount(arenaName);
 
+                    // 🔥 STATUS
                     String status = "§aWAITING";
-
-                    if (players > 0) status = "§cINGAME";
-                    if (players >= 4) status = "§8FULL";
+                    if (players >= 4) status = "§cFULL";
+                    if (players > 0 && players < 4) status = "§eINGAME";
 
                     sign.setLine(2, status);
-                    sign.setLine(3, "§7" + players + " hráčů");
+                    sign.setLine(3, "§b" + players + " hráčů");
 
                     sign.update();
                 }
-            }
+            });
         });
     }
 }
